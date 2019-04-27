48ba14265360d753dd5e7bc0d8422f9a61abbec7
Topher McCulloch <tophermcculloch@gmail.com>
2019-04-27 17:40:01 -0500

    updating footer

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
 
 					</div><!-- .site-info -->
 
