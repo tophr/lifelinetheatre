05620745329d70ea011a892e0397ba65199953e8
Topher McCulloch <tophermcculloch@gmail.com>
2019-04-28 10:56:33 -0500

    remove old editor functions

--- functions.php
+++ functions.php
@@ -21,7 +21,6 @@ $understrap_includes = array(
 	'/custom-comments.php',                 // Custom Comments file.
 	'/jetpack.php',                         // Load Jetpack compatibility file.
 	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker.
-	'/editor.php',                          // Load Editor functions.
 	'/deprecated.php',                      // Load deprecated functions.
 );
 
