ce5848094d6c7b5933ee504585a22342394635ec
Topher McCulloch <tophermcculloch@gmail.com>
2019-04-27 22:44:29 -0500

    front page template work

--- front-page.php
+++ front-page.php
@@ -26,14 +26,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<?php while ( have_posts() ) : the_post(); ?>
 
-						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-						?>
+						<?php the_content(); ?>
 
 					<?php endwhile; // end of the loop. ?>
 
