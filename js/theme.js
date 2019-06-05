db305a81c6abbd469eb3f5d2ec0691c60221a2e3
Topher McCulloch <tophermcculloch@gmail.com>
2019-06-04 22:55:18 -0500

    spinning wheels on dropdowns

--- js/theme.js
+++ js/theme.js
@@ -7052,6 +7052,7 @@
 (function($) {
 	
 // Flyout Menus	
+
 $('#main-menu > .dropdown > .dropdown-menu > .dropdown > a').on("click", function(e) {
     var submenu = $(this);
 	console.log('this');
@@ -7065,6 +7066,22 @@ $('.dropdown').on("hidden.bs.dropdown", function() {
     // hide any open menus when parent closes
     $('.dropdown-menu.show').removeClass('show');
 });
+	
+/*function toggleDropdown (e) {
+  const _d = $(e.target).closest('.dropdown'),
+    _m = $('.dropdown-menu', _d);
+  setTimeout(function(){
+    const shouldOpen = e.type !== 'click' && _d.is(':hover');
+    _m.toggleClass('show', shouldOpen);
+    _d.toggleClass('show', shouldOpen);
+    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+  }, e.type === 'mouseleave' ? 100 : 0);
+}
+
+$('body')
+  .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+  .on('click', '.dropdown-menu a', toggleDropdown);	
+*/
 	
 // Adding hash to tabs	
 $(document).ready(() => {
