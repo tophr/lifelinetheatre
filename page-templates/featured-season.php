e327e9362508130b0ae1e22c0bdaa139c998f1f8
Topher McCulloch <tophermcculloch@gmail.com>
2019-06-27 20:38:28 -0500

    featured season formatting

--- page-templates/featured-season.php
+++ page-templates/featured-season.php
@@ -39,15 +39,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<?php 
 					
-					$featured_season = get_field('featured_season');
-					
+					$featured_season = get_field('featured_season');					
 					$taxonomy = array( "name" => 'season' , "slug" => 'season');
 					$custom_post_type = "production";
-					
-					?>
-
-					<?php
-					
+										
 					$args = array(
 						'orderby'	=> 'name', 
 						'order'		=> 'ASC',
@@ -59,67 +54,68 @@ $container = get_theme_mod( 'understrap_container_type' );
 					foreach ( $terms as $category ) {
 						?>
 
-					<div class="archive-production-year">
-						<div class="row">
-						
+					<div class="featured-production-year">										
 
-							<?php
-							global $post; // Access the global $post object.
+						<?php
+						global $post; // Access the global $post object.
 
-							// Setup query to return each custom post within this taxonomy category
-							$o_queried_posts = get_posts( array(
-								'nopaging' => true,
-								'post_type' => $custom_post_type,
-								'taxonomy' => $category->taxonomy,
-								'term' => $category->slug,
-								'order' => 'ASC',
-								//'orderby' => 'title',
-							) );
-							?>
+						// Setup query to return each custom post within this taxonomy category
+						$o_queried_posts = get_posts( array(
+							'nopaging' => true,
+							'post_type' => $custom_post_type,
+							'taxonomy' => $category->taxonomy,
+							'term' => $category->slug,
+							'order' => 'ASC',
+							//'orderby' => 'title',
+						) );
 
-							<div class='archive-content col-sm-9'>
+						$productions = array();
+
+						foreach ( $o_queried_posts as $post ) {
+							$venue = get_field( 'venue', $post->id );
+							$productions[ $venue ][] = $post;
+						}
+
+						wp_reset_query();
+
+						foreach ( $productions as $production => $venue_title ) {
+							?>	
+
+							<h3 class="featured-venue-name">
+								<?php echo esc_html($production); ?>
+							</h3>
+
+							<div class="row">
+								<?php
+								foreach ( $venue_title as $listing => $single_listing ) {
+									setup_postdata( $single_listing );
+									$post_id = $single_listing->ID;
+									$title = get_the_title( $post_id );
+									$link = get_the_permalink( $post_id );
+									$dates = get_field('short_dates', $post_id);
+									?>
+									<div class="col-sm-4">
+										<div class="production-box">
+											<h4 class="entry-title">
+												<a href="<?php echo $link; ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php echo $title; ?></a>
+											</h4>
+
+											<?php if ( has_post_thumbnail() ) : ?>
+												<img src="<?php echo get_the_post_thumbnail_url( $post_id, 'large' ); ?>" class="production-thumbnail" />
+											<?php endif; ?>
+
+											<div class="production-date"><?php echo $dates; ?></div>
+										</div>	
+									</div>	
 
 								<?php
-								$productions = array();
-
-								foreach ( $o_queried_posts as $post ) {
-									$venue = get_field( 'venue', $post->id );
-									$productions[ $venue ][] = $post;
 								}
-
-								wp_reset_query();
-
-								foreach ( $productions as $production => $venue_title ) {
-									?>	
-								<div class="row archive-venue-row">
-									<div class="col-sm-3">
-										<h3 class="archive-venue-name">
-											<?php echo esc_html($production); ?>
-										</h3>
-									</div>
-									<div class="col-sm-9">
-									<?php
-									foreach ( $venue_title as $listing => $single_listing ) {
-										setup_postdata( $single_listing );
-										$post_id = $single_listing->ID;
-										$title = get_the_title( $post_id );
-										$link = get_the_permalink( $post_id );
-										$world_premiere = get_field( 'world_premiere', $post_id );
-										?>
-									<h4 class="entry-title">
-										<a href="<?php echo $link; ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php echo $title; ?></a><?php if ($world_premiere == 'adaptation') { echo '*'; } else if ($world_premiere == 'work') { echo '**'; }; ?>
-									</h4>
-
-									<?php
-									}
-									wp_reset_postdata(); ?>
-									</div></div>
-
-								<?php }
-								?>
-
-								</div>				
+								wp_reset_postdata(); ?>
 							</div>
+
+						<?php }
+						?>
+
 						</div>	
 					
 					<?php } // foreach( $categories as $category ) ; ?>					
