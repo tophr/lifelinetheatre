fe564ec7f6fab4d6e39c235fe108359b3e79b71f
Topher McCulloch <tophermcculloch@gmail.com>
2019-06-27 21:11:59 -0500

    blog styles tuneup

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	<?php echo get_the_post_thumbnail( $post->ID, 'full' ); ?>
 
 	<div class="entry-content">
 
