3749b9e347469e241dc07aaccc06761994984ed4
Topher McCulloch <tophermcculloch@gmail.com>
2019-12-18 22:41:23 -0600

    Updates widget areas

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * The header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="content">
- *
- * @package understrap
- */
+* The header for our theme.
+*
+* Displays all of the <head> section and everything up till <div id="content">
+*
+* @package understrap
+*/
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+  exit; // Exit if accessed directly.
 }
 
 $container = get_theme_mod( 'understrap_container_type' );
@@ -16,80 +16,79 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	
-<?php include_once("inc/analytics.php") ?>		
 
-<div class="site" id="page">
-	
-	<header id="header">
-		<div class="<?php echo esc_attr( $container ); ?>">
-			
-			<div class="row">
-				<div class="col-sm-4 header-branding">
-					<?php if ( ! has_custom_logo() ) { ?>
+  <?php include_once("inc/analytics.php") ?>
 
-					<?php if ( is_front_page() && is_home() ) : ?>
+  <div class="site" id="page">
 
-						<h1 class="mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+    <header id="header">
+      <div class="<?php echo esc_attr( $container ); ?>">
 
-					<?php else : ?>
+        <div class="row">
+          <div class="col-sm-4 header-branding">
+            <?php if ( ! has_custom_logo() ) { ?>
 
-						<a class="" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+              <?php if ( is_front_page() && is_home() ) : ?>
 
-					<?php endif; ?>
+                <h1 class="mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+
+              <?php else : ?>
+
+                <a class="" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+
+              <?php endif; ?>
 
 
-				<?php } else {
-					the_custom_logo();
-				} ?>					
-				</div>
-				
-				<div class="col-sm-8 text-right header-info">
-					<a href="https://boxoffice.diamondticketing.com/lifelinetheatre/events" target="_blank" class="btn btn-sm btn-primary"><strong>Buy Tickets</strong></a>
-					<div class="social-links">
-						<a href="http://visitor.r20.constantcontact.com/d.jsp?llr=dmdmnpbab&p=oi&m=1101102714243&sit=rvw8sq8ab" target="_blank" title="Join our email list"><i class="fas fa-envelope-square"></i></a> 
-						<a href="http://www.facebook.com/pages/Lifeline-Theatre/55661357652" target="_blank" title="Find us on Facebook!"><i class="fab fa-facebook"></i></a> <a href="http://twitter.com/lifelinetheatre" target="_blank"  title="Follow us on Twitter" ><i class="fab fa-twitter-square"></i></a> 
-					</div>
-					<a href="https://boxoffice.printtixusa.com/lifelinetheatre/donate" target="_blank" class="cta-donate"><div class="donate-line-1">It starts with <strong>YOU</strong></div><div class="donate-line-2">Join our family of supporters today!</div></a>					
-				</div>
-				
-			</div>
-		</div>
-	</header>
-	
-	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+            <?php } else {
+              the_custom_logo();
+            } ?>
+          </div>
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+          <div class="col-sm-8 text-right header-info">
+            <?php dynamic_sidebar( 'right-header' ); ?>
+            <div class="social-links">
+              <a href="http://visitor.r20.constantcontact.com/d.jsp?llr=dmdmnpbab&p=oi&m=1101102714243&sit=rvw8sq8ab" target="_blank" title="Join our email list"><i class="fas fa-envelope-square"></i></a>
+              <a href="http://www.facebook.com/pages/Lifeline-Theatre/55661357652" target="_blank" title="Find us on Facebook!"><i class="fab fa-facebook"></i></a> <a href="http://twitter.com/lifelinetheatre" target="_blank"  title="Follow us on Twitter" ><i class="fab fa-twitter-square"></i></a>
+            </div>            
+          </div>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        </div>
+      </div>
+    </header>
 
-			<div class="<?php echo esc_attr( $container ); ?>">
+    <div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+      <a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav justify-content-between',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 3,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-			</div>		
+      <nav class="navbar navbar-expand-md navbar-dark bg-primary">
 
-		</nav>
+        <div class="<?php echo esc_attr( $container ); ?>">
 
-	</div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <?php wp_nav_menu(
+            array(
+              'theme_location'  => 'primary',
+              'container_class' => 'collapse navbar-collapse',
+              'container_id'    => 'navbarNavDropdown',
+              'menu_class'      => 'navbar-nav justify-content-between',
+              'fallback_cb'     => '',
+              'menu_id'         => 'main-menu',
+              'depth'           => 3,
+              'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+            )
+          ); ?>
+        </div>
+
+      </nav>
+
+    </div>
