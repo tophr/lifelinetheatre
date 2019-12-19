b0c5424fea5f9301e0d553c268f0d90937e81601
Topher McCulloch <tophermcculloch@gmail.com>
2019-12-18 22:56:01 -0600

    Allows awards for productions

--- page-templates/productions.php
+++ page-templates/productions.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Template Name: Production History
- *
- * The template for displaying production history.
- *
- * @package understrap
- */
+* Template Name: Production History
+*
+* The template for displaying production history.
+*
+* @package understrap
+*/
 
 if ( !defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+  exit; // Exit if accessed directly.
 }
 
 get_header();
@@ -21,119 +21,124 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="page-wrapper">
 
-	<?php if ( have_posts() ) : ?>
+  <?php if ( have_posts() ) : ?>
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+    <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
-		<div class="row">
+      <div class="row">
 
-			<div class="col-md-9 order-md-last content-area" id="primary">
+        <div class="col-md-9 order-md-last content-area" id="primary">
 
-				<main class="site-main" id="main">
+          <main class="site-main" id="main">
 
-					<p>*World premiere of Original Literary Adaptation<br> **World premiere of Original Work</p>
+            <p>*World premiere of Original Literary Adaptation<br> **World premiere of Original Work</p>
 
-					<?php $taxonomy = array( "name" => 'season' , "slug" => 'season');
-						$custom_post_type = "production";
+            <?php $taxonomy = array( "name" => 'season' , "slug" => 'season');
+            $custom_post_type = "production";
 
-						if ( have_posts() )
-						the_post();
-						?>
+            if ( have_posts() )
+            the_post();
+            ?>
 
-					<?php
-					// Query your specified taxonomy to get, in order, each category
-					$categories = get_terms( $taxonomy[ 'name' ], 'orderby=title&order=DESC' );
-					foreach ( $categories as $category ) {
-						?>
+            <?php
+            // Query your specified taxonomy to get, in order, each category
+            $categories = get_terms( $taxonomy[ 'name' ], 'orderby=title&order=DESC' );
+            foreach ( $categories as $category ) {
+              ?>
 
-					<div class="archive-production-year">
-						<div class="row">
+              <div class="archive-production-year">
+                <div class="row">
 
-							<div class="col-sm-3">
-								<h2 class="">
-									<?php echo $category->name; ?>
-								</h2>
-							</div>	
+                  <div class="col-sm-3">
+                    <h2 class="">
+                      <?php echo $category->name; ?>
+                    </h2>
+                  </div>
 
-							<?php
-							global $post; // Access the global $post object.
+                  <?php
+                  global $post; // Access the global $post object.
 
-							// Setup query to return each custom post within this taxonomy category
-							$o_queried_posts = get_posts( array(
-								'nopaging' => true,
-								'post_type' => $custom_post_type,
-								'taxonomy' => $category->taxonomy,
-								'term' => $category->slug,
-								'order' => 'ASC',
-								//'orderby' => 'title',
-							) );
-							?>
+                  // Setup query to return each custom post within this taxonomy category
+                  $o_queried_posts = get_posts( array(
+                    'nopaging' => true,
+                    'post_type' => $custom_post_type,
+                    'taxonomy' => $category->taxonomy,
+                    'term' => $category->slug,
+                    'order' => 'ASC',
+                    //'orderby' => 'title',
+                  ) );
+                  ?>
 
-							<div class='archive-content col-sm-9'>
+                  <div class='archive-content col-sm-9'>
 
 
-								<?php
-								$productions = array();
+                    <?php
+                    $productions = array();
 
-								foreach ( $o_queried_posts as $post ) {
-									$venue = get_field( 'venue', $post->id );
-									$productions[ $venue ][] = $post;
-								}
+                    foreach ( $o_queried_posts as $post ) {
+                      $venue = get_field( 'venue', $post->id );
+                      $productions[ $venue ][] = $post;
+                    }
 
-								wp_reset_query();
+                    wp_reset_query();
 
-								foreach ( $productions as $production => $venue_title ) {
-									?>	
-								<div class="row archive-venue-row">
-									<div class="col-sm-3">
-										<h3 class="archive-venue-name">
-											<?php echo esc_html($production); ?>
-										</h3>
-									</div>
-									<div class="col-sm-9">
-									<?php
-									foreach ( $venue_title as $listing => $single_listing ) {
-										setup_postdata( $single_listing );
-										$post_id = $single_listing->ID;
-										$title = get_the_title( $post_id );
-										$link = get_the_permalink( $post_id );
-										$world_premiere = get_field( 'world_premiere', $post_id );
-										?>
-									<h4 class="entry-title">
-										<a href="<?php echo $link; ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php echo $title; ?></a><?php if ($world_premiere == 'adaptation') { echo '*'; } else if ($world_premiere == 'work') { echo '**'; }; ?>
-									</h4>
+                    foreach ( $productions as $production => $venue_title ) {
+                      ?>
+                      <div class="row archive-venue-row">
+                        <div class="col-sm-3">
+                          <h3 class="archive-venue-name">
+                            <?php echo esc_html($production); ?>
+                          </h3>
+                        </div>
+                        <div class="col-sm-9">
+                          <?php
+                          foreach ( $venue_title as $listing => $single_listing ) {
+                            setup_postdata( $single_listing );
+                            $post_id = $single_listing->ID;
+                            $title = get_the_title( $post_id );
+                            $link = get_the_permalink( $post_id );
+                            $awards = get_field( 'awards', $post_id );
+                            $world_premiere = get_field( 'world_premiere', $post_id );
+                            ?>
+                            <h4 class="entry-title">
+                              <a href="<?php echo $link; ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php echo $title; ?></a><?php if ($world_premiere == 'adaptation') { echo '*'; } else if ($world_premiere == 'work') { echo '**'; }; ?>
+                            </h4>
 
-									<?php
-									}
-									wp_reset_postdata(); ?>
-									</div></div>
+                            <?php if ( $awards ): ?>
+                              <p><?php echo $awards ?></p>
+                            <?php endif; ?>
 
-								<?php }
-								?>
+                            <?php
+                          }
+                          wp_reset_postdata(); ?>
+                        </div></div>
 
-								</div>				
-							</div>
-						</div>	
-					
-					<?php } // foreach( $categories as $category ) ; ?>
-					
-					<?php // General page content / manual production entry ?>
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-					
-				</main>
+                      <?php }
+                      ?>
 
-			</div>
-			
-			<div class="col-md-3 order-md-first widget-area" id="left-sidebar" role="complementary">
-				<?php get_template_part( 'sidebar-templates/sidebar', 'subnav' ); ?>
-			</div>
+                    </div>
+                  </div>
+                </div>
 
-		</div>
+              <?php } // foreach( $categories as $category ) ; ?>
 
-	</div>
+              <?php // General page content / manual production entry ?>
+              <?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-	<?php endif; ?>
+            </main>
 
-</div> <!-- #page-wrapper -->
+          </div>
 
-<?php get_footer(); ?>
+          <div class="col-md-3 order-md-first widget-area" id="left-sidebar" role="complementary">
+            <?php get_template_part( 'sidebar-templates/sidebar', 'subnav' ); ?>
+          </div>
+
+        </div>
+
+      </div>
+
+    <?php endif; ?>
+
+  </div> <!-- #page-wrapper -->
+
+  <?php get_footer(); ?>
